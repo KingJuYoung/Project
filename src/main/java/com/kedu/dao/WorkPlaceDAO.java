03787d966feeb93704f51651108a4fba4d6e6977
KingJuYoung <dlawndud6544@gmail.com>
2026-04-02 00:39:45 +0900

    DAO 로직 구현 테스트X

--- src/main/java/com/kedu/dao/WorkPlaceDAO.java
+++ src/main/java/com/kedu/dao/WorkPlaceDAO.java
@@ -12,8 +12,21 @@ public class WorkPlaceDAO {
 	public JdbcTemplate jdbc;
 	
 	public int insertToworkpalce(WorkPlaceDTO dto) {
-		String sql = "insert into workplace values ()";
-		return jdbc.update(sql);
+		String sql = "insert into workplace ("
+                + "seq, id, name, pay_per_hour, pay_type, pay_cycle, payday, "
+                + "tax_applied, insurance_applied, work_start_time, work_end_time, workplace_date"
+                + ") values (workplace_seq.nextval, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, systimestamp)";
+		return jdbc.update(sql,
+                dto.getId(),
+                dto.getName(),
+                dto.getPay_per_hour(),
+                dto.getPay_type(),
+                dto.getPay_cycle(),
+                dto.getPayday(),
+                dto.getTax_applied(),
+                dto.getInsurance_applied(),
+                dto.getWork_start_time(),
+                dto.getWork_end_time());
 	}
 	
 	
