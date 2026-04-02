919758773a22f34c124d771c25bd81df9f1a9be5
킹주영 <dlawndud6544@naver.com>
2026-04-02 12:08:58 +0900

    pull 전 커밋

--- src/main/java/com/kedu/controllers/WorkPlaceController.java
+++ src/main/java/com/kedu/controllers/WorkPlaceController.java
@@ -18,8 +18,9 @@ public class WorkPlaceController {
 	
 	@RequestMapping("/insert")
 	public String insert(WorkPlaceDTO dto, HttpSession session) {
-		String memberId = (String) session.getAttribute("loggedInId");
+		String memberId = (String) session.getAttribute("loginId");
 	    dto.setId(memberId);
+	    System.out.println(memberId);
 	    if(dto.getPay_type() == null) dto.setPay_type("시급");
 	    
 	    if (dto.getWork_start_time() != null && dto.getWork_start_time().isEmpty()) {
@@ -27,6 +28,26 @@ public class WorkPlaceController {
 	    }
 	    if (dto.getWork_end_time() != null && dto.getWork_end_time().isEmpty()) {
 	        dto.setWork_end_time(null);
+	    }
+	    if (dto.getEmployment_insurance() == null) {
+	        dto.setEmployment_insurance("N");
+	    }
+	    String taxVal = dto.getTax_applied();
+	    if (taxVal == null || taxVal.equals("") || taxVal.equals("custom")) {
+	        dto.setTax_applied("0");
+	    }
+	    
+	    String insVal = dto.getInsurance_applied();
+
+	    if ("4".equals(insVal)) {
+	        dto.setInsurance_applied("Y");
+	        dto.setEmployment_insurance("Y");
+	    } else if ("1".equals(insVal)) {
+	        dto.setInsurance_applied("N");
+	        dto.setEmployment_insurance("Y");
+	    } else {
+	        dto.setInsurance_applied("N");
+	        dto.setEmployment_insurance("N");
 	    }
 	    
 		dao.insertToworkpalce(dto);
